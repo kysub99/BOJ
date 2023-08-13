6de7c6e1cb12ff7ce94194f787292d8c247aa109
kysub99 <115832836+kysub99@users.noreply.github.com>
2023-08-13 19:42:11 +0900

    230718

---
+++ 14888.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	for (int i : arr) {
+		cin >> arr[i];
+	}
+	int pl, mi, mu, di;
+	cin >> pl >> mi >> mu >> di;
+
+}
